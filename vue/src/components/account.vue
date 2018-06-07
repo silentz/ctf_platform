523c2480a8c909a5d25a348fe94305fca867c6bb
Max Pershin <maxim.pershin@protonmail.com>
2018-06-07 14:23:07 +0300

    Added template for account component

---
+++ vue/src/components/account.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+    <div class="account">
+        <div class="header">
+            <h2>{{ username }}</h2>
+        </div>
+        <div class="content">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Account",
+    computed: {
+        username: function() {
+            return this.$store.state.username;
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.account {
+    margin: 10px;
+    padding: 10px;
+    background-color: white;
+
+
+}
+</style>
